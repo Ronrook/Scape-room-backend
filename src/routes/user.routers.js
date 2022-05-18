c9529d4aadb1c8fa072983fb992264010e8e121a
Ronald Rosero <ronaldes93@gmail.com>
2022-05-18 14:53:59 -0500

    crud users

--- src/routes/user.routers.js
+++ src/routes/user.routers.js
@@ -7,6 +7,17 @@ import {authJwt, verifySignup} from '../middlewares'
 
 router.get('/getuser', [authJwt.verifyToken], userCtrl.getUserByToken)
 
-router.post('/', [authJwt.verifyToken, authJwt.isAdmin, verifySignup.checkRolesExisted], userCtrl.createUser)
+router.get('/:userId', [authJwt.verifyToken, authJwt.isAdmin ], userCtrl.getUserById)
+
+router.get('/getusers', [authJwt.verifyToken, authJwt.isAdmin ], userCtrl.getAllUsers)
+
+router.get('/getadmins', [authJwt.verifyToken, authJwt.isAdmin ], userCtrl.getAllAdmins)
+
+router.put('/:userId', [authJwt.verifyToken, authJwt.isAdmin ], userCtrl.updateUserById)
+
+router.delete("/:userId", [authJwt.verifyToken, authJwt.isAdmin], userCtrl. deleteUserById);
+
+
+// router.post('/', [authJwt.verifyToken, authJwt.isAdmin, verifySignup.checkRolesExisted], userCtrl.createUser)
 
 export default router;
