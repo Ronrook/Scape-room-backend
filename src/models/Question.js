74ad3f08ddfe50ec731bcebdc5a096ee589dad62
marvintt <marvintt@hotmail.com>
2022-05-23 17:23:44 -0500

    Update Question.js

--- src/models/Question.js
+++ src/models/Question.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const questionSchema = new mongoose.Schema({
+  question_number: Number,
+  group: String,
   question: String,
   correct_answer: String,
   incorrect_answers: Array,
   tips: Array,
+}, {
+  timestamps: true,
+  versionKey: false
 });
 
 const Question = mongoose.model("Questions", questionSchema);
