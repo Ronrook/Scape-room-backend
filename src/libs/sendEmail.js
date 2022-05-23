74bafd9e56950cb59af12c67adaf071abaca9403
Ronald Rosero <ronaldes93@gmail.com>
2022-05-23 17:25:53 -0500

    send data for excel deploying

--- src/libs/sendEmail.js
+++ src/libs/sendEmail.js
@@ -3,18 +3,17 @@ import { transporter } from './mailer';
 
 const sendEmail = async (user)  => {
     const { username, email, password} = user
-try {
-    // send mail with defined transport object
-    await transporter.sendMail({
-    from: '"Fred Foo" <jdquimbayo72@misena.edu.co>', // sender address
-    to: email, // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: `Hola ${username} tus credenciales de acceso son  
-    email:${email} 
-    passsword:${password}`, // plain text body
-    html: "<b>Hello world?</b>", // html body
-    })
-        
+    try {
+        // send mail with defined transport object
+        await transporter.sendMail({
+        from: '"Fred Foo" <jdquimbayo72@misena.edu.co>', // sender address
+        to: email, // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: `Hola ${username} tus credenciales de acceso son  
+        email:${email} 
+        passsword:${password}`, // plain text body
+        html: "<b>Hello world?</b>", // html body
+        })
     } catch (error) {
         return 'error al envial email'
         
