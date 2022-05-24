7d5efcbb6431bb6796feb943b0b439dfdb29bbf5
Ronald Rosero <ronaldes93@gmail.com>
2022-05-24 16:23:11 -0500

    test logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -51,35 +51,35 @@ export const signin = async (req, res) =>{
 
     if (!matchPassword) return res.status(401).json({token: null, message: 'password invalido'})
 
-    // const token =  jwt.sign({id: userFound._id}, config.SECRET, {
-    //     expiresIn: 3600 // 1 hora
-    // } )
-
-
-    // res.status(200).json({token})
-
-
-    const role = await Role.find({_id: {$in: userFound.roles}})
-    
-    console.log(userFound.isLoggedIn)
-    
-    if (!userFound.isLoggedIn) {
-
-
-        if(role[0].name === 'user'){
-                userFound.isLoggedIn = true
-                await userFound.save();
-                console.log('es un usuario')
-                console.log(userFound.isLoggedIn)
-        }
-
-        const token =  jwt.sign({id: userFound._id}, config.SECRET, {
+    const token =  jwt.sign({id: userFound._id}, config.SECRET, {
         expiresIn: 3600 // 1 hora
-        } )
+    } )
 
-        res.status(200).json({token})
-    }
 
-    if (userFound.isLoggedIn) return res.status(401).json({token: null, message: 'No puedes ingresar por segunda vez'})
+    res.status(200).json({token})
+
+
+    // const role = await Role.find({_id: {$in: userFound.roles}})
+    
+    // console.log(userFound.isLoggedIn)
+    
+    // if (!userFound.isLoggedIn) {
+
+
+    //     if(role[0].name === 'user'){
+    //             userFound.isLoggedIn = true
+    //             await userFound.save();
+    //             console.log('es un usuario')
+    //             console.log(userFound.isLoggedIn)
+    //     }
+
+    //     const token =  jwt.sign({id: userFound._id}, config.SECRET, {
+    //     expiresIn: 3600 // 1 hora
+    //     } )
+
+    //     res.status(200).json({token})
+    // }
+
+    // if (userFound.isLoggedIn) return res.status(401).json({token: null, message: 'No puedes ingresar por segunda vez'})
 
 }
