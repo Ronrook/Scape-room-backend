8483bbc080eb33d243f5b603ce94f270db07ce17
Ronald Rosero <ronaldes93@gmail.com>
2022-05-24 12:15:48 -0500

    excel const data

--- src/controllers/excel.controller.js
+++ src/controllers/excel.controller.js
@@ -10,7 +10,6 @@ export const saveDataExcel = async (req, res) => {
     
 
     
-    
     // const excel = XLSX.readFile(
         
     //     'D:/Desktop/scape_room_backend/src/usuarios.xlsx'
@@ -18,7 +17,6 @@ export const saveDataExcel = async (req, res) => {
 
     // const nombreHoja = excel.SheetNames; 
     // const data = XLSX.utils.sheet_to_json(excel.Sheets[nombreHoja[0]]);
-
 
 
 
