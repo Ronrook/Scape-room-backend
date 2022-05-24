0cd9b89702e49759ba96b207d33305efe33a6152
Ronald Rosero <ronaldes93@gmail.com>
2022-05-24 15:55:12 -0500

    is logged in

--- src/controllers/excel.controller.js
+++ src/controllers/excel.controller.js
@@ -3,24 +3,18 @@ import {  createUser }  from '../controllers/user.controller'
 const sendEmail = require('../libs/sendEmail')
 
 export const saveDataExcel = async (req, res) => {
-    // const {urlExcel} = req.body;
+    const data = req.body;
 
 
-    const excel = XLSX.readFile(
-        
-        'D:/Desktop/scape_room_backend/src/usuarios.xlsx'
-    );
-
-    const nombreHoja = excel.SheetNames; 
-    const datos = XLSX.utils.sheet_to_json(excel.Sheets[nombreHoja[0]]);
+    
 
     
     try {
 
-        datos.forEach(async user => {
+        data.forEach(async user => {
 
-            const respuesta = await validateCreateuser(user)
-            console.log(respuesta)
+            const verify = await validateCreateuser(user)
+            console.log(verify)
 
             if (!verify) {
                 const newUser = await  createUser(user)
