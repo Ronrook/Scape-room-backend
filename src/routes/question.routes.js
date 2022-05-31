50aa8850e35f2bd8cdbeb02c52c9ae0895f09fe4
Ronald Rosero <ronaldes93@gmail.com>
2022-05-31 17:26:43 -0500

    update routes questions

--- src/routes/question.routes.js
+++ src/routes/question.routes.js
@@ -5,8 +5,8 @@ import * as questionsCtrl from '../controllers/questions.controller'
 
 import { authJwt } from '../middlewares'
 
-router.get('/', [authJwt.verifyToken, authJwt.isAdmin], questionsCtrl.getQuestions)
-router.get('/:questionId', [authJwt.verifyToken, authJwt.isAdmin], questionsCtrl.getQuestionById)
+router.get('/', [authJwt.verifyToken], questionsCtrl.getQuestions)
+router.get('/:questionId', [authJwt.verifyToken], questionsCtrl.getQuestionById)
 
 router.put('/:questionId', [authJwt.verifyToken, authJwt.isAdmin], questionsCtrl.updateQuestionById)
 
