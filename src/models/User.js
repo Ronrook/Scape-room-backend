e30aac6fa32f4e664f71cd7d9e80a5c218e49520
Ronald Rosero <ronaldes93@gmail.com>
2022-05-31 10:23:34 -0500

    Question code

--- src/models/User.js
+++ src/models/User.js
@@ -47,4 +47,5 @@ userSchema.statics.comparePassword = async (password, receivedPassword) => {
     return await bcrypt.compare(password, receivedPassword)
 }
 
+
 export default model("User", userSchema);
